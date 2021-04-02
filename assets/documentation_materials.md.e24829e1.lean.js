fdb7742853b0c4de8960b9180a061ba779b7343e
stirante <brzozowski.s.piotr@gmail.com>
2021-04-02 20:46:51 +0000

    Deploying to gh-pages from @ Bedrock-OSS/bedrock-wiki-vite@6319df9181ec2c0e77c8d4d541530fd91c327655 🚀

---
+++ assets/documentation_materials.md.e24829e1.lean.js
@@ -0,0 +1,1 @@
+import{o as t,c as d,d as r}from"./app.86985e37.js";const e='{"title":"Materials","description":"","frontmatter":{"layout":"page","title":"Materials","parent":"Documentation"},"relativePath":"documentation/materials.md","lastUpdated":1617396302960}',a={},_=r('',4);a.render=function(r,e,a,i,n,l){return t(),d("div",null,[_])};export default a;export{e as __pageData};
