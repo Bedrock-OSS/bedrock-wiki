e399a28be69c57a2b3881ca0f30126bdfdb8f308
solvedDev <33347616+solvedDev@users.noreply.github.com>
2021-04-03 22:02:43 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki-vite@8a084dd5446043914e7e62f4e6d4010e4d50b556 🚀

--- assets/knowledge_nbt_commands.md.b7ec77fb.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as o,a as e}from"./app.e2885f98.js";const u='{"title":"NBT Commands","description":"","frontmatter":{"layout":"page","title":"NBT Commands","parent":"Knowledge"},"relativePath":"knowledge/nbt_commands.md","lastUpdated":1617487141807}',q={},n=e('',34);q.render=function(e,u,q,a,r,l){return t(),o("div",null,[n])};export default q;export{u as __pageData};
