a3dba6eb2e372a73a63f39a38b05bdd701b6dcbd
Joelant05 <64587014+Joelant05@users.noreply.github.com>
2021-04-03 15:56:31 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki-vite@d2dc6ab83b1439065285afce630fc24e206e59b7 🚀

--- assets/knowledge_nbt_commands.md.fdd04651.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as o,a as e}from"./app.4a348b8b.js";const u='{"title":"NBT Commands","description":"","frontmatter":{"layout":"page","title":"NBT Commands","parent":"Knowledge"},"relativePath":"knowledge/nbt_commands.md","lastUpdated":1617465200947}',q={},a=e('',34);q.render=function(e,u,q,n,r,_){return t(),o("div",null,[a])};export default q;export{u as __pageData};
