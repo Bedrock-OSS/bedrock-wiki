b84397301ae699530709762fbef336f4e301dac1
solvedDev <33347616+solvedDev@users.noreply.github.com>
2021-04-03 20:45:45 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki-vite@5ec9aab88a2ef621d7942e662ebfab67fccd9be6 🚀

--- assets/documentation_materials.md.97e109cf.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as d,a as r}from"./app.8b820bd7.js";const e='{"title":"Materials","description":"","frontmatter":{"layout":"page","title":"Materials","parent":"Documentation"},"relativePath":"documentation/materials.md","lastUpdated":1617481127480}',a={},_=r('',4);a.render=function(r,e,a,i,n,l){return t(),d("div",null,[_])};export default a;export{e as __pageData};
