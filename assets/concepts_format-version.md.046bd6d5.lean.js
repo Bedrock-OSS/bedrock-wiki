ebcf59126e1f6c8d639d10ea385b405a9ef258e1
solvedDev <33347616+solvedDev@users.noreply.github.com>
2021-04-03 17:15:23 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki-vite@fdf6ccd105c02d87613afb49e6f0dfd6c1a4ed4b 🚀

---
+++ assets/concepts_format-version.md.046bd6d5.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,a}from"./app.3b73e831.js";const t='{"title":"Format Versions","description":"","frontmatter":{"layout":"page","title":"Format Versions","parent":"Concepts"},"relativePath":"concepts/format-version.md","lastUpdated":1617470009218}',o={},e=a('',6);o.render=function(a,t,o,p,r,i){return n(),s("div",null,[e])};export default o;export{t as __pageData};
