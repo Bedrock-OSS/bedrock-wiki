7124d207dad30c6a156d5d90f56be81de9f9b5f2
Joelant05 <64587014+Joelant05@users.noreply.github.com>
2021-04-03 15:59:10 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki-vite@fb22a04f5b6f2d2d687ee2e7b90da67b345ce059 🚀

--- assets/documentation_materials.md.20104747.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as d,a as r}from"./app.34506d70.js";const e='{"title":"Materials","description":"","frontmatter":{"layout":"page","title":"Materials","parent":"Documentation"},"relativePath":"documentation/materials.md","lastUpdated":1617465274704}',a={},_=r('',4);a.render=function(r,e,a,i,n,l){return t(),d("div",null,[_])};export default a;export{e as __pageData};
