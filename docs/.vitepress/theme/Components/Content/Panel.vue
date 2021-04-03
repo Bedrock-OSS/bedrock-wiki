d4746028e0aaea89da07b09676ceeaf04c0e45d3
Joelant05 <Joelant05@users.noreply.github.com>
2021-04-03 13:33:46 +0100

    upd: simplify Panel component further

--- docs/.vitepress/theme/Components/Content/Panel.vue
+++ docs/.vitepress/theme/Components/Content/Panel.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="panel-container">
-		<p :class="`panel-${type}`">
+		<p :class="`panel ${type}`">
 			<slot />
 		</p>
 	</div>
@@ -20,63 +20,39 @@ const type = toRef(props, 'type')
 	padding: 10px 5px 10px 5px;
 }
 
-.panel-info {
+.panel {
 	-moz-border-radius: 6px;
 	-webkit-border-radius: 6px;
+	background-size: 32px, 32px;
+	background-position: 5px 15px;
+	background-repeat: no-repeat;
+	border-radius: 6px;
+	overflow: hidden;
+	padding: 13px 20px 15px 50px;
+	min-height: 80px;
+}
+
+.info {
 	background-color: #e8f7ff; /* Needs to use theme color variable */
 	background-image: url(/assets/images/icons/info.png);
-	background-size: 32px, 32px;
-	background-position: 5px 15px;
-	background-repeat: no-repeat;
 	border: solid 1px #5bccee; /* Needs to use theme color variable */
-	border-radius: 6px;
-	overflow: hidden;
-	padding: 13px 20px 15px 50px;
-	min-height: 80px;
 }
 
-.panel-warning {
-	-moz-border-radius: 6px;
-	-webkit-border-radius: 6px;
+.warning {
 	background-color: #fffbe8; /* Needs to use theme color variable */
 	background-image: url(/assets/images/icons/warning.png);
-	background-size: 32px, 32px;
-	background-position: 5px 15px;
-	background-repeat: no-repeat;
 	border: solid 1px #ee7d5b; /* Needs to use theme color variable */
-	border-radius: 6px;
-	overflow: hidden;
-	padding: 13px 20px 15px 50px;
-	min-height: 80px;
 }
 
-.panel-error {
-	-moz-border-radius: 6px;
-	-webkit-border-radius: 6px;
+.error {
 	background-color: #ffe1e1; /* Needs to use theme color variable */
 	background-image: url(/assets/images/icons/error.png);
-	background-size: 32px, 32px;
-	background-position: 5px 15px;
-	background-repeat: no-repeat;
 	border: solid 1px #ee5b5b; /* Needs to use theme color variable */
-	border-radius: 6px;
-	overflow: hidden;
-	padding: 13px 20px 15px 50px;
-	min-height: 80px;
 }
 
-.panel-notice {
-	-moz-border-radius: 6px;
-	-webkit-border-radius: 6px;
+.notice {
 	background-color: #f8e8ff; /* Needs to use theme color variable */
 	background-image: url(/assets/images/icons/notice.png);
-	background-size: 32px, 32px;
-	background-position: 5px 15px;
-	background-repeat: no-repeat;
 	border: solid 1px #ee5bd5; /* Needs to use theme color variable */
-	border-radius: 6px;
-	overflow: hidden;
-	padding: 13px 20px 15px 50px;
-	min-height: 80px;
 }
 </style>
