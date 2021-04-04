1f854811c469f5483737901205d170c559c2f5e9
solvedDev <solvedDev@gmail.com>
2021-04-04 13:16:20 +0200

    upd: attempt at loading 404 markdown file

--- docs/.vitepress/theme/Types/VueShim.d.ts
+++ docs/.vitepress/theme/Types/VueShim.d.ts
@@ -3,3 +3,8 @@ declare module '*.vue' {
 	const component: DefineComponent<{}, {}, any>
 	export default component
 }
+declare module '*.md' {
+	import { DefineComponent } from 'vue'
+	const component: DefineComponent<{}, {}, any>
+	export default component
+}
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,7 +3,8 @@ import FilePath from './Components/Content/FilePath.vue'
 import Label from './Components/Content/Label.vue'
 import YouTubeEmbed from './Components/Content/YouTubeEmbed.vue'
 import { Theme } from 'vitepress'
-import { Component, watchEffect } from 'vue'
+import { Component } from 'vue'
+import Component404 from '../../404.md'
 import './Styles/styles'
 
 // Add global components to this array to register them automatically
@@ -16,7 +17,7 @@ const globalComponents: [string, Component][] = [
 const theme: Theme = {
 	Layout,
 	// @ts-ignore
-	NotFound: () => '404', //Custom 404
+	NotFound: () => Component404, //Custom 404
 	enhanceApp({ app, router, siteData }) {
 		for (const [componentName, globalComponent] of globalComponents) {
 			app.component(componentName, globalComponent)
