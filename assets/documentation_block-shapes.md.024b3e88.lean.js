3914d1e8b35b255026bafc90cc86f5b20aedcfe5
SirLich <lrkoehler77@gmail.com>
2021-04-04 11:04:22 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki-vite@8c727001d0eb2751e6aaf96d3bd508d93475f12f 🚀

---
+++ assets/documentation_block-shapes.md.024b3e88.lean.js
@@ -0,0 +1,1 @@
+import{o as t,c as r,d}from"./app.9136b780.js";const e='{"title":"Block Shapes","description":"","frontmatter":{"layout":"page","title":"Block Shapes","parent":"Documentation"},"relativePath":"documentation/block-shapes.md","lastUpdated":1617534095264}',a={},o=d('',3);a.render=function(d,e,a,l,c,s){return t(),r("div",null,[o])};export default a;export{e as __pageData};
---
+++ assets/knowledge_nbt_commands.md.54c4e635.lean.js
@@ -0,0 +1,1 @@
+import{o as t,c as o,d as e}from"./app.9136b780.js";const u='{"title":"NBT Commands","description":"","frontmatter":{"layout":"page","title":"NBT Commands","parent":"Knowledge"},"relativePath":"knowledge/nbt_commands.md","lastUpdated":1617534095268}',q={},n=e('',34);q.render=function(e,u,q,a,r,l){return t(),o("div",null,[n])};export default q;export{u as __pageData};
