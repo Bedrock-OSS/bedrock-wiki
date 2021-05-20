684a53041d93fe3d53c4c14f84f547d878f3627a
solvedDev <33347616+solvedDev@users.noreply.github.com>
2021-05-20 10:53:41 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki-vite@6ed367913fbe97983a182513bfebdf8d7deca56a 🚀

---
+++ assets/app.47a5cca6.js
@@ -0,0 +1,1 @@
+export{f as createApp}from"./404.md.fcf056d5.js";
--- assets/documentation_biome-tags.md.1cfcc95f.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as e,d as a}from"./404.md.2366fddb.js";const d='{"title":"Biome Tags","description":"","frontmatter":{"layout":"page","title":"Biome Tags","parent":"Documentation"},"relativePath":"documentation/biome-tags.md","lastUpdated":1621468687695}',r={},_=a('',5);r.render=function(a,d,r,o,l,s){return t(),e("div",null,[_])};export default r;export{d as __pageData};
