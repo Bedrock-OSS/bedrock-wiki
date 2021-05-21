e26ff9c7ce9365426edfee2470abaa1751326cfc
solvedDev <33347616+solvedDev@users.noreply.github.com>
2021-05-21 12:07:27 +0200

    feat: finish theme dropdown
    closes #12

--- docs/.vitepress/theme/Components/Controls/DarkMode.vue
+++ docs/.vitepress/theme/Components/Controls/DarkMode.vue
@@ -1,35 +1,58 @@
 <template>
-	<div class="absolute inset-y-0 left-0 pl-3 flex items-center">
-		<span class="leading-4">
-			<Icon class="pointer-events-none w-4 h-4" />
-		</span>
+	<div class="relative">
+		<CurrentThemeIcon
+			class="absolute opacity-70 top-2 left-2"
+			width="19"
+			height="19"
+		/>
+		<select
+			name="theme"
+			v-model="themeId"
+			class="
+				appearance-none
+				outline-none
+				select-none
+				leading-4
+				rounded-md
+				border-1 border-gray-200
+				focus:border-blue-300
+				focus:ring focus:ring-blue-200 focus:ring-opacity-50
+				dark:bg-dark-gray-900
+				dark:border-dark-gray-800
+				text-sm
+				py-2
+				px-8
+				block
+			"
+		>
+			<option value="light">Light</option>
+			<option value="dark">Dark</option>
+			<option value="system">System</option>
+		</select>
+		<ChevronLeftIcon
+			class="absolute opacity-70 top-1 right-1 transform -rotate-90"
+		/>
 	</div>
-	<select
-		name="theme"
-		v-model="themeId"
-		class="
-			select-none
-			leading-4
-			rounded
-			border-gray-300
-			rounded-md
-			focus:border-blue-300
-			focus:ring focus:ring-blue-200 focus:ring-opacity-50
-			dark:bg-dark-gray-900
-			dark:border-dark-gray-800
-			text-sm
-			py-2
-			pl-8
-			block
-		"
-	>
-		<option value="light">Light</option>
-		<option value="dark">Dark</option>
-		<option value="system">System</option>
-	</select>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useTheme } from '../../Composables/theme'
+import ChevronLeftIcon from '../Icons/ChevronLeftIcon.vue'
+import DesktopMacIcon from '../Icons/DesktopMacIcon.vue'
+import LightThemeIcon from '../Icons/LightThemeIcon.vue'
+import DarkThemeIcon from '../Icons/DarkThemeIcon.vue'
+
 const { themeId } = useTheme()
+
+const CurrentThemeIcon = computed(() => {
+	switch (themeId.value) {
+		case 'system':
+			return DesktopMacIcon
+		case 'dark':
+			return DarkThemeIcon
+		case 'light':
+			return LightThemeIcon
+	}
+})
 </script>
