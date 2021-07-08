43be3340c42356d375dd4683f9acd99a704d7597
sermah <40694307+sermah@users.noreply.github.com>
2021-07-08 17:05:05 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki-vite@d8114369d159fc11fd00b802f2fd8405d2bb1626 🚀

--- assets/documentation_index.md.b4dac016.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as e}from"./404.md.f491001b.js";const o='{"title":"Documentation","description":"","frontmatter":{"title":"Documentation"},"relativePath":"documentation/index.md","lastUpdated":1625761318679}',n={};n.render=function(o,n,i,r,a,d){return t(),e("div")};export default n;export{o as __pageData};
---
+++ assets/scripting_index.md.7e25f6b9.js
@@ -0,0 +1,1 @@
+import{o as t,c as i}from"./404.md.ffd38386.js";const r='{"title":"Scripting","description":"","frontmatter":{"title":"Scripting"},"relativePath":"scripting/index.md","lastUpdated":1625763862861}',e={};e.render=function(r,e,n,d,o,a){return t(),i("div")};export default e;export{r as __pageData};
