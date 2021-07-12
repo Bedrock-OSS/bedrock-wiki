37cb3e23a968889b8e7d270e50e84c6823b6738d
sermah <2004mah@gmail.com>
2021-07-12 21:45:42 +0000

    We are using heroicons, aren't we?

--- docs/.vitepress/theme/Components/Icons/CopyIcon.vue
+++ docs/.vitepress/theme/Components/Icons/CopyIcon.vue
@@ -1,7 +1,5 @@
 <template>
-	<svg class="icon" width="24" height="24" viewBox="0 0 24 24">
-		<path
-			d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"
-		/>
+	<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
 	</svg>
 </template>
--- docs/.vitepress/theme/Components/Icons/DesktopMacIcon.vue
+++ docs/.vitepress/theme/Components/Icons/DesktopMacIcon.vue
@@ -1,7 +1,5 @@
 <template>
-	<svg class="icon" width="24" height="24" viewBox="0 0 24 24">
-		<path
-			d="M21,14H3V4H21M21,2H3C1.89,2 1,2.89 1,4V16A2,2 0 0,0 3,18H10L8,21V22H16V21L14,18H21A2,2 0 0,0 23,16V4C23,2.89 22.1,2 21,2Z"
-		/>
+	<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
 	</svg>
 </template>
