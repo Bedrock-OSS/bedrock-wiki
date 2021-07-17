0e516628d708cbc0da216ddc11886b0a12138ed1
SmokeyStack <seansyahril04@gmail.com>
2021-07-17 14:58:25 -0400

    Fixed a dead link in 404
    Could possibly resolve the 404 issue?

--- docs/.vitepress/theme/Components/Pages/NotFound.vue
+++ docs/.vitepress/theme/Components/Pages/NotFound.vue
@@ -4,7 +4,7 @@
             <h1 class="!block">404 - Page not found!</h1>
             <p>Don't type <code>/spawnpoint</code> here.</p>
             <img src="/assets/images/misc/dead_bush.png">
-            <p>You want to create a page for the wiki? Feel free to contribute! <a href="/contribute">Here's how.</a></p>
+            <p>You want to create a page for the wiki? Feel free to contribute! <a href="/bedrock-wiki-vite/contribute">Here's how.</a></p>
         </div>
     </div>
 </template>
