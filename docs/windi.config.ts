a801366293eb761bd9c7610d5ae6f280df6d935a
destruc7i0n <destruc7i0n@users.noreply.github.com>
2021-08-11 17:38:49 -0400

    youtube embed: make responsive

--- docs/windi.config.ts
+++ docs/windi.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'windicss/helpers'
 // const colors = require('tailwindcss/colors')
 
 export default defineConfig({
-	plugins: [require('windicss/plugin/typography')],
+	plugins: [
+		require('windicss/plugin/typography'),
+		require('windicss/plugin/aspect-ratio'),
+	],
 
 	extract: {
 		include: ['.vitepress/**/*.{vue,ts}'],
