cd4272d925959fb59d0a6876b0ee0b5dd040de64
SmokeyStack <84206875+SmokeyStack@users.noreply.github.com>
2021-09-07 14:47:00 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki@e02beb73e1615f1dbdd11224d34f760c25c37f40 🚀

--- assets/app.2b053bee.js
+++
@@ -1,1 +0,0 @@
-export{p as createApp}from"./404.md.bc6159fa.js";
---
+++ assets/commands_index.md.6f150619.js
@@ -0,0 +1,1 @@
+import{o as t,c as e}from"./404.md.6a35c198.js";const a='{"title":"Commands","description":"","frontmatter":{"title":"Commands"},"relativePath":"commands/index.md","lastUpdated":1631025872282}',d={};d.render=function(a,d,o,r,m,n){return t(),e("div")};export default d;export{a as __pageData};
