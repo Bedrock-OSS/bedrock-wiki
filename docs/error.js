a607b7aa50b45665497f449b00588e916a476282
stirante <brzozowski.s.piotr@gmail.com>
2021-10-31 22:58:37 +0100

    Remove useless logs

--- docs/error.js
+++ docs/error.js
@@ -5,7 +5,6 @@ export default function errorUtility() {
     name: 'error-utility',
     configureServer(server) {
       return () => {
-        console.log(server.middlewares.eventNames());
         server.middlewares.use(function (err, req, res, next) {
           console.log(`::error file=${err.loc.file},line=${err.loc.line},col=${err.loc.column}::Error in ${path.basename(err.loc.file)} at line ${err.loc.line}: ${err.message}`);
           next(err)
