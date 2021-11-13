bc7cc4ada39df746ec1a5181797db35b71da9db0
SirLich <lrkoehler77@gmail.com>
2021-11-13 15:02:48 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki@67f265f765c48ae52e7fd390a82cf3040d0f9fa8 🚀

--- assets/scripting_index.md.d40c3fb3.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as e}from"./404.md.40c96e87.js";const i='{"title":"Scripting","description":"","frontmatter":{"title":"Scripting"},"relativePath":"scripting/index.md","lastUpdated":1636721349629}',r={};r.render=function(i,r,n,d,o,a){return t(),e("div")};export default r;export{i as __pageData};
