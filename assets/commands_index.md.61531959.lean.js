2f1cc9596422c4e710cbf65bf2b20b4727b6721a
MedicalJewel105 <87320205+MedicalJewel105@users.noreply.github.com>
2021-11-14 18:00:53 +0000

    Deploying to generated from @ Bedrock-OSS/bedrock-wiki@6a437ee3e0d6c82db5f51d83dc7e492cf40d5f06 🚀

--- assets/commands_index.md.61531959.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as e}from"./404.md.edab3c8e.js";const d='{"title":"Commands","description":"","frontmatter":{"title":"Commands"},"relativePath":"commands/index.md","lastUpdated":1636912377550}',a={};a.render=function(d,a,o,r,m,n){return t(),e("div")};export default a;export{d as __pageData};
--- assets/scripting_index.md.3a3546f0.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as e}from"./404.md.edab3c8e.js";const i='{"title":"Scripting","description":"","frontmatter":{"title":"Scripting"},"relativePath":"scripting/index.md","lastUpdated":1636912377790}',r={};r.render=function(i,r,n,d,a,o){return t(),e("div")};export default r;export{i as __pageData};
