f649e0db056192410a20fe0a9cba318cf9c80dfe
SirLich <lrkoehler77@gmail.com>
2022-01-27 21:45:58 +0100

    Attempt fix

--- docs/.vitepress/theme/Components/Pages/NotFound.vue
+++ docs/.vitepress/theme/Components/Pages/NotFound.vue
@@ -4,10 +4,6 @@
 			<h1 class="!block">404 - Page not found!</h1>
 			<p>Don't type <code>/spawnpoint</code> here.</p>
 			<img src="/assets/images/misc/dead_bush.png" />
-			<p>
-				You want to create a page for the wiki? Feel free to contribute!
-				<a :href="site.value + 'contribute'">Here's how.</a>
-			</p>
 		</div>
 	</div>
 </template>
