9f91e225483d01d740fda7a5897b1ef5cff55d89
MJ105 <eldiarlopatkin@gmail.com>
2022-04-03 09:39:43 +0300

    Update

--- docs/.vitepress/theme/Components/Pages/Homepage.vue
+++ docs/.vitepress/theme/Components/Pages/Homepage.vue
@@ -19,7 +19,7 @@
 				/>
 			</div>
 			<div class="ml-4 flex justify-center flex-col flex-nowrap">
-				<h1 class="mt-0">Bugrock Wiki</h1>
+				<h1 class="mt-0">Bedrock Wiki</h1>
 				<p>
 					This wiki is a knowledge-sharing website for Technical
 					Bedrock, containing documentation, tutorials, and general
@@ -66,11 +66,12 @@
 				"
 			>
 				<CardLink
-					title="Click!"
-					imgsrc="assets/april/minecoin.png"
-					link="april/fool-4"
+					title="Beginner's Guide"
+					imgsrc="assets/images/homepage/crafting_table_0.png"
+					link="guide/introduction"
 				>
-					Claim your Minecoins Airdop!
+					Step-by-step tutorials, intended as your first-contact with
+					addons.
 				</CardLink>
 				<CardLink
 					title="Discord"
@@ -80,41 +81,53 @@
 					Join the discord to learn about addons, and seek help from
 					other addon creators.
 				</CardLink>
-				<CardLink
-					title="Learn More!"
-					imgsrc="assets/april/gear.webp"
-					link="https://old-wiki.bedrock.dev//scripting/hello-world-tutorial"
+				<Card
+					title="Entities"
+					imgsrc="assets/images/homepage/spawn_egg_30.png"
 				>
-					New, powerful and fast engine. Learn more about M-scripting!
-				</CardLink>
-				<CardLink
-					title="???"
-					imgsrc="assets/april/what.png"
-					link="/april/fool-1"
+					<a href="entities/entities-intro">Beginner's Guide</a>:
+					Learn about the structure of Behavior Pack Entity files.<br /><br />
+					<a href="entities/troubleshooting-entities"
+						>Troubleshooting</a
+					>: Learn to troubleshoot common issues when creating
+					entities, such as invisible textures.
+				</Card>
+				<Card
+					title="Items"
+					imgsrc="assets/images/homepage/iron_pickaxe_0.png"
 				>
-					???
-				</CardLink>
-				<CardLink
-					title="Shaders 2.0"
-					imgsrc="assets/april/render_dragon.webp"
-					link="/april/fool-6"
+					<a href="items/items-intro">Beginner's Guide</a>: A "Hello
+					world" guide in creating your first item.<br /><br />
+					<a href="items/items-16">Items 1.16+</a>: Learn about the
+					new experimental items.
+				</Card>
+				<Card
+					title="Blocks"
+					imgsrc="assets/images/homepage/diamond_ore_0.png"
 				>
-					New era of shaders has came. No one believed.
-				</CardLink>
-				<CardLink
-					title="Quintuple Ultimate Add-On v.2"
-					imgsrc="assets/april/ultimate_addon.png"
-					link="/april/fool-5"
+					<a href="blocks/blocks-intro">Beginner's Guide</a>: A "Hello
+					world" guide in making blocks.<br /><br />
+					<a href="blocks/blocks-16">Blocks 1.16+</a>: Learn about the
+					new experimental blocks.
+				</Card>
+				<Card
+					title="Scripting"
+					imgsrc="assets/images/homepage/scripting.png"
 				>
-					⚠ You must try this addon...
-				</CardLink>
-				<CardLink
-					title="Make a Chair"
-					imgsrc="assets/april/chair.png"
-					link="/april/fool-3"
+					<a href="scripting/scripting-intro.html">Beginner's Guide</a
+					>: Learn the basics of the legacy scripting-API<br /><br />
+					<a href="scripting/game-tests.html">Game Test Framework</a>:
+					Learn about the new scripting-API!
+				</Card>
+				<Card
+					title="World Generation"
+					imgsrc="assets/images/homepage/buildplate.png"
 				>
-					What to do when you buy a chair?
-				</CardLink>
+					<a href="world-generation/world-generation-intro"
+						>Beginner's Guide</a
+					>: Learn about creation of custom biomes, structures, ores
+					and more.<br /><br />
+				</Card>
 				<CardLink
 					title="Contribute"
 					imgsrc="assets/images/homepage/writable_book_0.png"
