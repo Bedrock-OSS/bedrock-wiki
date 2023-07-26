8ff53c95afcb534ca9ba1999c8196ede2ae32b14
QuazChick <quazchicken@outlook.com>
2023-07-26 13:02:26 +0100

    Update tag code and styling

--- docs/.vitepress/theme/Components/Navigation/NavLinkTag.vue
+++ docs/.vitepress/theme/Components/Navigation/NavLinkTag.vue
@@ -1,48 +1,26 @@
 <template>
 	<span
 		v-if="valid"
-		:class="`${colour} label`"
+		:class="`${colour} label ml-auto`"
 	>{{ title }}</span>
 </template>
 
 <script setup lang="ts">
+import { colorFromTag } from '../../Composables/colorFromTag'
+
+const displayedTags = [
+  'guide',
+  'help',
+  'info',
+  'outdated',
+  'system'
+]
 
 const props = defineProps<{
 	tag: string
 }>()
 
-let valid = true
-let title = ''
-let colour = ''
-
-switch (props.tag) {
-case 'guide': {
-	title = 'GUIDE'
-	colour = 'green'
-	break
-}
-case 'outdated': {
-	title = 'OUTDATED'
-	colour = 'red'
-	break
-}
-case 'info': {
-	title = 'INFO'
-	colour = 'green'
-	break
-}
-case 'system': {
-	title = 'SYSTEM'
-	colour = 'blue'
-	break
-}
-case 'help': {
-	title = 'HELP'
-	colour = 'blue'
-	break
-}
-default:
-	valid = false
-	break
-}
+let valid = displayedTags.includes(props.tag)
+let title = props.tag
+let colour = colorFromTag(props.tag)
 </script>
---
+++ docs/.vitepress/theme/Composables/colorFromTag.ts
@@ -0,0 +1,30 @@
+export function colorFromTag(tag: string) {
+  let color = ''
+
+  switch (tag) {
+    case 'guide':
+      color = 'green'
+      break
+    case 'help':
+    case 'easy':
+    case 'beginner':
+    case 'recipe':
+    case 'info':
+    case 'system':
+      color = 'blue'
+      break
+    case 'experimental':
+    case 'intermediate':
+      color = 'orange'
+      break
+    case 'beta':
+    case 'expert':
+    case 'outdated':
+    case 'red':
+      color = 'red'
+      break
+    default:
+  }
+
+  return color
+}
