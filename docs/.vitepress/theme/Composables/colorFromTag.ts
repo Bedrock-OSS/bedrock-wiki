b70dbcdff0d497b3b72d9cf6d189ab41428c36c2
QuazChick <quazchicken@outlook.com>
2023-07-27 15:44:54 +0100

    "Correct" indentation

--- docs/.vitepress/theme/Composables/colorFromTag.ts
+++ docs/.vitepress/theme/Composables/colorFromTag.ts
@@ -2,31 +2,31 @@ export function colorFromTag(tag: string) {
 	let color = ''
 
 	switch (tag) {
-		case 'guide':
-			color = 'green'
-			break
-		case 'help':
-		case 'easy':
-		case 'beginner':
-		case 'recipe':
-		case 'info':
-		case 'system':
-			color = 'blue'
-			break
-		case 'experimental':
-		case 'intermediate':
-			color = 'orange'
-			break
-		case 'beta':
-		case 'expert':
-		case 'outdated':
-		case 'red':
-			color = 'red'
-			break
-		case 'scripting':
-			color = 'yellow'
-			break
-		default:
+	case 'guide':
+		color = 'green'
+		break
+	case 'help':
+	case 'easy':
+	case 'beginner':
+	case 'recipe':
+	case 'info':
+	case 'system':
+		color = 'blue'
+		break
+	case 'experimental':
+	case 'intermediate':
+		color = 'orange'
+		break
+	case 'beta':
+	case 'expert':
+	case 'outdated':
+	case 'red':
+		color = 'red'
+		break
+	case 'scripting':
+		color = 'yellow'
+		break
+	default:
 	}
 
 	return color
