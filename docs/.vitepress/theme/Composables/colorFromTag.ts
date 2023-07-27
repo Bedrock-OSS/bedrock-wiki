11808839466c1ba381efff0bb2d79418c13037ed
QuazChick <quazchicken@outlook.com>
2023-07-27 15:42:27 +0100

    Use tabs for indentation

--- docs/.vitepress/theme/Composables/colorFromTag.ts
+++ docs/.vitepress/theme/Composables/colorFromTag.ts
@@ -1,33 +1,33 @@
 export function colorFromTag(tag: string) {
-  let color = ''
+	let color = ''
 
-  switch (tag) {
-    case 'guide':
-      color = 'green'
-      break
-    case 'help':
-    case 'easy':
-    case 'beginner':
-    case 'recipe':
-    case 'info':
-    case 'system':
-      color = 'blue'
-      break
-    case 'experimental':
-    case 'intermediate':
-      color = 'orange'
-      break
-    case 'beta':
-    case 'expert':
-    case 'outdated':
-    case 'red':
-      color = 'red'
-      break
-    case 'scripting':
-      color = 'yellow'
-      break
-    default:
-  }
+	switch (tag) {
+		case 'guide':
+			color = 'green'
+			break
+		case 'help':
+		case 'easy':
+		case 'beginner':
+		case 'recipe':
+		case 'info':
+		case 'system':
+			color = 'blue'
+			break
+		case 'experimental':
+		case 'intermediate':
+			color = 'orange'
+			break
+		case 'beta':
+		case 'expert':
+		case 'outdated':
+		case 'red':
+			color = 'red'
+			break
+		case 'scripting':
+			color = 'yellow'
+			break
+		default:
+	}
 
-  return color
+	return color
 }
