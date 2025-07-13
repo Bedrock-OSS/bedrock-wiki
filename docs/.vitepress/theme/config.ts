315a605346bbc26eb7d7cc50d759ea023d5d7ffb
QuazChick <quazchicken@outlook.com>
2025-07-13 19:13:37 +0100

    Moved plugins to dedicated folder

--- docs/.vitepress/theme/config.ts
+++ docs/.vitepress/theme/config.ts
@@ -1,31 +1,9 @@
 import { defineConfigWithTheme } from "vitepress";
-import taskListsPlugin from "markdown-it-task-lists";
 
 import { ThemeConfig, WikiConfig } from "./types";
 import head, { transformHead } from "./head";
-import * as shiki from "shiki";
 import languages from "./languages";
-
-// Used for inline code block syntax highlighting, this is necessary as by default the custom highlighter used does not import the built-in languages.
-const inlineCodeBlockSyntaxHighlightingLanguages: shiki.LanguageRegistration[] = [];
-for (const language of Object.values(shiki.bundledLanguages)) {
-  const languageRegistration = (await language()).default[0];
-  /**
-   * Allow for using the custom JSON language definition from {@link languages}.
-   */
-  if (languageRegistration.name === "json") {
-    continue;
-  }
-  inlineCodeBlockSyntaxHighlightingLanguages.push(languageRegistration);
-}
-inlineCodeBlockSyntaxHighlightingLanguages.push(...languages);
-const lightTheme = (await shiki.bundledThemes["light-plus"]()).default;
-const darkTheme = (await shiki.bundledThemes["dark-plus"]()).default;
-const highlighter = shiki.createHighlighterCoreSync({
-  engine: shiki.createJavaScriptRegexEngine(),
-  themes: [lightTheme, darkTheme],
-  langs: inlineCodeBlockSyntaxHighlightingLanguages,
-});
+import plugins from "./plugins";
 
 const isFastBuild = process.env.FAST_BUILD?.trim() === "true";
 
@@ -100,25 +78,7 @@ export function defineWikiConfig(config: WikiConfig) {
         light: "light-plus",
       },
       config(md) {
-        md.use(taskListsPlugin, { label: true });
-
-        // 8Crafter's inline code block syntax highlighting plugin.
-        md.renderer.rules.code_inline = (tokens, idx) => {
-          const code = tokens[idx].content;
-          const highlighted = highlighter.codeToHtml(code, {
-            lang: tokens[idx].attrGet("lang") ?? "",
-            themes: { light: lightTheme, dark: darkTheme },
-            structure: "inline",
-            defaultColor: false,
-          });
-          return `${
-            (tokens[idx].attrGet("noLeftCodeBlock") ?? "false") === "true"
-              ? ""
-              : '<code class="shiki">'
-          }${highlighted}${
-            (tokens[idx].attrGet("noRightCodeBlock") ?? "false") === "true" ? "" : "</code>"
-          }`;
-        };
+        for (const plugin of plugins) md.use(plugin);
       },
     },
 
---
+++ docs/.vitepress/theme/plugins/index.ts
@@ -0,0 +1,4 @@
+import taskListsPlugin from "markdown-it-task-lists";
+import { inlineHighlightPlugin } from "./inlineHighlight";
+
+export default [inlineHighlightPlugin, taskListsPlugin];
